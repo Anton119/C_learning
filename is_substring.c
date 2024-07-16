a1e8f8a064fd4c9f087ec2c366daa48a236a32a8
Anton_Wombat <153746663+Anton119@users.noreply.github.com>
2024-07-16 19:48:08 +0300

    upgraded a func

--- is_substring.c
+++ is_substring.c
@@ -9,32 +9,32 @@ int s_len(char* s);
 
 int main () {
 
-char* s1 = "abc";
-char* s2 = "qwabc42";
+char* s1 = "abc4";
+char* s2 = "qwabc4c2";
 printf("%d", is_substring(s1, s2));
+//is_substring(s1,s2);
 
 return 0;
 }
 
-int s_len(char* s){
-for(int i=0; ;i+=1) {
-    if (s[i] == '\0') {
-        return i;
-    }
-}
-
-}
 
 
 int is_substring(char* s1, char* s2) {
-
-for(int i=0; i<=s_len(s1); i+=1) {
-    for (int j=0; j<=s_len(s2); j+=1) {
-        if(s1[i] == s2[j]) {
-        return 1;
+int i,j;
+for(i=0; s2[i]!='\0'; i++) {
+    if (s2[i] == s1[0]){
+        for(j=0; s1[j]!='\0'; j++) {
+            if(s2[i+j] != s1[j]) {
+                break;
+                }
         }
+        if(s1[j] == '\0'){
+            return 1;
+            }
+        }
+        
+        
     }
-
-}
 return 0;
+
 }
